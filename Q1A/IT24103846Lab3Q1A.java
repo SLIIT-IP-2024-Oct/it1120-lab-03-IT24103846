061621914bf71fa4d4591ecffaf3325248708e5f
IT24103846 <it24103846@my.sliit.lk>
2024-11-01 10:14:06 +0530

    Update IT24103846Lab3Q1A.java

--- Q1A/IT24103846Lab3Q1A.java
+++ Q1A/IT24103846Lab3Q1A.java
@@ -4,14 +4,14 @@ public class IT24103846Lab3Q1A {
 
 	Scanner input = new Scanner (System.in);
 	
-	System.out.println ("Enter the price of 1kg rice:");	
+	System.out.print ("Enter the price of 1kg rice: ");	
 	double pricePerKg = input.nextDouble();
 
-	System.out.println ("Enter the number of kg you want to buy:");
+	System.out.print ("Enter the number of kg you want to buy: ");
 	double kilograms = input.nextDouble();
 	double totalprice = pricePerKg * kilograms;
 
-	System.out.println ("The total amount is:" + totalprice) ;
+	System.out.print ("The total amount is: " + totalprice) ;
 	
 	}
 }
